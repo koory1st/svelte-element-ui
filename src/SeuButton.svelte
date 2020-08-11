0b37c6d2596042620295cfb3231b3b9c8f5b8452
koory1st <32436334@qq.com>
2020-08-11 22:48:34 +0800

    use css in button

--- src/SeuButton.svelte
+++ src/SeuButton.svelte
@@ -1,8 +1,6 @@
 <script>
+  import SeuButtonStyle from './SeuButtonStyle.svelte';
   import { get } from 'svelte/store';
-  import { styled } from 'SvelteStyledSystem';
-  import { theme } from './theme.js';
-  import buttonStyle from './SeuButtonStyle';
   export let type = 'default';
   export let size;
   export let icon = '';
@@ -13,130 +11,31 @@
   export let round;
   export let circle;
 
-  let styles = {};
+  let classList = ['seu-button'];
 
   $: {
-    console.log('buttonStyle', buttonStyle);
-    // default
-    if (type === 'default' && !plain && !disabled) {
-      styles = { ...buttonStyle.default.notPlain.notDisabled };
-    }
-
-    if (type === 'default' && !plain && disabled) {
-      styles = { ...buttonStyle.default.notPlain.disabled };
-    }
-    if (type === 'default' && plain && !disabled) {
-      styles = { ...buttonStyle.default.plain.notDisabled };
-    }
-
-    if (type === 'default' && plain && disabled) {
-      styles = { ...buttonStyle.default.plain.disabled };
-    }
-
-    // primary
-    if (type === 'primary' && !plain && !disabled) {
-      styles = { ...buttonStyle.primary.notPlain.notDisabled };
-    }
-
-    if (type === 'primary' && !plain && disabled) {
-      styles = { ...buttonStyle.primary.notPlain.disabled };
-    }
-    if (type === 'primary' && plain && !disabled) {
-      styles = { ...buttonStyle.primary.plain.notDisabled };
-    }
-
-    if (type === 'primary' && plain && disabled) {
-      styles = { ...buttonStyle.primary.plain.disabled };
-    }
-
-    // success
-    if (type === 'success' && !plain && !disabled) {
-      styles = { ...buttonStyle.success.notPlain.notDisabled };
-    }
-
-    if (type === 'success' && !plain && disabled) {
-      styles = { ...buttonStyle.success.notPlain.disabled };
-    }
-    if (type === 'success' && plain && !disabled) {
-      styles = { ...buttonStyle.success.plain.notDisabled };
-    }
-
-    if (type === 'success' && plain && disabled) {
-      styles = { ...buttonStyle.success.plain.disabled };
-    }
-
-    // info
-    if (type === 'info' && !plain && !disabled) {
-      styles = { ...buttonStyle.info.notPlain.notDisabled };
-    }
-
-    if (type === 'info' && !plain && disabled) {
-      styles = { ...buttonStyle.info.notPlain.disabled };
-    }
-    if (type === 'info' && plain && !disabled) {
-      styles = { ...buttonStyle.info.plain.notDisabled };
-    }
-
-    if (type === 'info' && plain && disabled) {
-      styles = { ...buttonStyle.info.plain.disabled };
-    }
-
-    // warning
-    if (type === 'warning' && !plain && !disabled) {
-      styles = { ...buttonStyle.warning.notPlain.notDisabled };
-    }
-
-    if (type === 'warning' && !plain && disabled) {
-      styles = { ...buttonStyle.warning.notPlain.disabled };
-    }
-    if (type === 'warning' && plain && !disabled) {
-      styles = { ...buttonStyle.warning.plain.notDisabled };
-    }
-
-    if (type === 'warning' && plain && disabled) {
-      styles = { ...buttonStyle.warning.plain.disabled };
-    }
-
-    // danger
-    if (type === 'danger' && !plain && !disabled) {
-      styles = { ...buttonStyle.danger.notPlain.notDisabled };
-    }
-
-    if (type === 'danger' && !plain && disabled) {
-      styles = { ...buttonStyle.danger.notPlain.disabled };
-    }
-    if (type === 'danger' && plain && !disabled) {
-      styles = { ...buttonStyle.danger.plain.notDisabled };
-    }
-
-    if (type === 'danger' && plain && disabled) {
-      styles = { ...buttonStyle.danger.plain.disabled };
-    }
-
-    // text
-    if (type === 'text' && !disabled) {
-      styles = { ...buttonStyle.text.notDisabled };
-    }
-
-    if (type === 'text' && disabled) {
-      styles = { ...buttonStyle.text.disabled };
-    }
-
     if (disabled) {
-      styles.cursor = 'not-allowed';
+      classList.push('is-disabled');
     }
+
+    if (plain) {
+      classList.push('is-plain');
+    }
+
+    classList.push(`seu-button--${type || 'default'}`);
 
     if (round) {
-      styles.borderRadius = '20px';
+      classList.push('is-round');
     }
 
     if (circle) {
-      styles.borderRadius = '50%';
+      classList.push('is-circle');
     }
   }
 </script>
 
-<button class="seu-button" type={nativeType} use:styled={[styles, $theme]} disabled={disabled || loading} on:click>
+<SeuButtonStyle />
+<button class={classList.join(' ')} type={nativeType} disabled={disabled || loading} on:click>
   {#if loading}
     <i class="seu-icon-loading" />
   {/if}
