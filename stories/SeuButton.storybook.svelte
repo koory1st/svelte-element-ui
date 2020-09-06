f66802e933165750580312243d6561ffd340045d
koory1st <32436334@qq.com>
2020-09-06 22:55:02 +0800

    add button group

--- stories/SeuButton.storybook.svelte
+++ stories/SeuButton.storybook.svelte
@@ -1,5 +1,6 @@
 <script>
   import { SeuRow, SeuButton } from '../dist/bundle'
+  import SeuButtonGroup from '../src/SeuButtonGroup.svelte'
   export let type
   export let size
   export let icon = ''
@@ -11,10 +12,10 @@
   export let circle
 </script>
 
-<h2>Try It with Knobs below</h2>
+<h1>Try It with Knobs below</h1>
 <SeuButton {type} {size} {disabled} {plain} {round} {circle} {loading} on:click>默认按钮</SeuButton>
 
-<h2>Samples</h2>
+<h1>Samples</h1>
 <SeuRow>
   <SeuButton>默认按钮</SeuButton>
   <SeuButton type="primary">主要按钮</SeuButton>
@@ -33,6 +34,7 @@
   <SeuButton type="danger" plain>危险按钮</SeuButton>
 </SeuRow>
 
+<h2>round</h2>
 <SeuRow>
   <SeuButton round>圆角按钮</SeuButton>
   <SeuButton type="primary" round>主要按钮</SeuButton>
@@ -42,6 +44,7 @@
   <SeuButton type="danger" round>危险按钮</SeuButton>
 </SeuRow>
 
+<h2>circle</h2>
 <SeuRow>
   <SeuButton icon="seu-icon-search" circle />
   <SeuButton type="primary" icon="seu-icon-edit" circle />
@@ -51,6 +54,7 @@
   <SeuButton type="danger" icon="seu-icon-delete" circle />
 </SeuRow>
 
+<h2>disabled</h2>
 <SeuRow>
   <SeuButton disabled>默认按钮</SeuButton>
   <SeuButton type="primary" disabled>主要按钮</SeuButton>
@@ -72,10 +76,23 @@
 <SeuButton type="text">文字按钮</SeuButton>
 <SeuButton type="text" disabled>文字按钮</SeuButton>
 
+<h2>group</h2>
+<SeuButtonGroup>
+  <SeuButton type="primary" icon="seu-icon-arrow-left">上一页</SeuButton>
+  <SeuButton type="primary">下一页<i class="seu-icon-arrow-right seu-icon--right" /></SeuButton>
+</SeuButtonGroup>
+<SeuButtonGroup>
+  <SeuButton type="primary" icon="seu-icon-edit" />
+  <SeuButton type="primary" icon="seu-icon-share" />
+  <SeuButton type="primary" icon="seu-icon-delete" />
+</SeuButtonGroup>
+
+<h2>loading</h2>
 <SeuRow>
   <SeuButton type="primary" loading={true}>加载中</SeuButton>
 </SeuRow>
 
+<h2>size</h2>
 <SeuRow>
   <SeuButton>默认按钮</SeuButton>
   <SeuButton size="medium">中等按钮</SeuButton>
@@ -88,6 +105,13 @@
   <SeuButton size="small" round>小型按钮</SeuButton>
   <SeuButton size="mini" round on:click>超小按钮</SeuButton>
 </SeuRow>
+
+<h2>icon</h2>
+<SeuButton type="primary" icon="seu-icon-edit" />
+<SeuButton type="primary" icon="seu-icon-share" />
+<SeuButton type="primary" icon="seu-icon-delete" />
+<SeuButton type="primary" icon="seu-icon-search">搜索</SeuButton>
+<SeuButton type="primary">上传<i class="seu-icon-upload seu-icon--right" /></SeuButton>
 
 <style>
   :global(.seu-row) {
