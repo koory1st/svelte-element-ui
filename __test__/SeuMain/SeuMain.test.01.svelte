5f3de2741ac7d313518826daa4a3cdd54363956e
koory1st <32436334@qq.com>
2020-11-15 00:19:34 +0800

    test(seu-main): add seu-main test

---
+++ __test__/SeuMain/SeuMain.test.01.svelte
@@ -0,0 +1,8 @@
+<script type="ts">
+  import SeuContainer from '../../src/SeuContainer.svelte'
+  import SeuMain from '../../src/SeuMain.svelte'
+</script>
+
+<SeuContainer>
+  <SeuMain class="abc" style="color:red;font-size:14px">seu main</SeuMain>
+</SeuContainer>
