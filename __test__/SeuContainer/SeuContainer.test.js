06f79526a7965b2b3e45e716255b7b955528996a
koory1st <32436334@qq.com>
2020-11-24 20:46:30 +0800

    test(seu-container): amend a test case

--- __test__/SeuContainer/SeuContainer.test.js
+++ __test__/SeuContainer/SeuContainer.test.js
@@ -6,17 +6,19 @@ import Comp01 from './SeuContainer.test.01.svelte'
 import Comp02 from './SeuContainer.test.02.svelte'
 
 describe('Test Container 01', () => {
-  const { getByText } = render(Comp01)
+  test('main class ', () => {
+    const { getByText } = render(Comp01)
 
-  expect(getByText('Test Container 01')).toHaveClass('seu-container')
-  expect(getByText('Test Container 01')).not.toHaveClass('is-vertical')
-  expect(getByText('Test Container 01')).toHaveClass('abc')
-  expect(getByText('Test Container 01')).toHaveStyle('color:red')
-  expect(getByText('Test Container 01')).toHaveStyle('font-size:14px')
+    expect(getByText('Test Container 01')).toHaveClass('seu-container')
+    expect(getByText('Test Container 01')).not.toHaveClass('is-vertical')
+    expect(getByText('Test Container 01')).toHaveClass('abc')
+    expect(getByText('Test Container 01')).toHaveStyle('color:red')
+    expect(getByText('Test Container 01')).toHaveStyle('font-size:14px')
+  })
 })
 
 describe('Test Container 02', () => {
-  test('main class', () => {
+  test('default class', () => {
     const { getByText } = render(Comp02)
 
     expect(getByText('Test Container 02')).toHaveClass('seu-container')
