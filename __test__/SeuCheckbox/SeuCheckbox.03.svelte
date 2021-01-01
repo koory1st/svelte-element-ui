3eef22fb7717210d5e931b2205c726d63dba7760
koory1st <32436334@qq.com>
2021-01-01 11:37:39 +0800

    test(checkbox): add disabled prop change test

---
+++ __test__/SeuCheckbox/SeuCheckbox.03.svelte
@@ -0,0 +1,10 @@
+<script type="ts">
+  import SeuCheckbox from '../../src/checkbox/SeuCheckbox.svelte'
+  let disabled = false
+</script>
+
+<div>
+  SeuCheckbox.03
+  <SeuCheckbox {disabled}>label3</SeuCheckbox>
+</div>
+<button on:click={() => (disabled = !disabled)}>button03</button>
