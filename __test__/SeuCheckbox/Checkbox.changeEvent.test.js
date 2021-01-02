70c61d3303891c78dd791ec0493d887ee4d7115c
koory1st <32436334@qq.com>
2021-01-02 19:30:10 +0800

    test(checkbox): add change event test

---
+++ __test__/SeuCheckbox/Checkbox.changeEvent.test.js
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/svelte'
+import { testElementAndResult } from '../util/TestUtil'
+import Comp from './Checkbox.changeEvent.svelte'
+
+test('change the value by click checkbox', async () => {
+  const { getByText } = render(Comp)
+  const testDom = getByText('test component')
+  const checkboxEle = testDom.querySelector('label')
+  testElementAndResult(testDom)
+
+  await fireEvent.click(checkboxEle)
+  testElementAndResult(testDom)
+})
