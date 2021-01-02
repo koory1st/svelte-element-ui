1e6631bd1507605b9af726e8ed71ba3470ad4183
koory1st <32436334@qq.com>
2021-01-02 19:48:34 +0800

    test(checkboxgroup): add change event test

---
+++ __test__/SeuCheckboxGroup/CheckboxGroup.changeEvent.svelte
@@ -0,0 +1,17 @@
+<script type="ts">
+  import SeuCheckboxGroup from '../../src/checkbox/SeuCheckboxGroup.svelte'
+  let group
+  let changedValue
+</script>
+
+<div>
+  test component
+  <span>changedValue:{changedValue}</span>
+  <span>group:{group}</span>
+  <SeuCheckboxGroup
+    options={['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']}
+    bind:group
+    on:change={event => {
+      changedValue = event.detail
+    }} />
+</div>
