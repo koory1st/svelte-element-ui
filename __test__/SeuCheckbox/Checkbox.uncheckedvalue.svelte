453ae3292c2873e28118f08dafbfbe6caea58dd2
koory1st <32436334@qq.com>
2021-01-02 11:59:26 +0800

    test(checkbox): add checkedvalue number test

---
+++ __test__/SeuCheckbox/Checkbox.uncheckedvalue.svelte
@@ -0,0 +1,13 @@
+<script type="ts">
+  import SeuCheckbox from '../../src/checkbox/SeuCheckbox.svelte'
+  let value
+</script>
+
+<div>
+  test component
+  <div id="value">{value}</div>
+  <SeuCheckbox checkedValue="yes" bind:value>label5</SeuCheckbox>
+</div>
+
+<button id="buttonYes" on:click={() => (value = 'yes')}>button-yes</button>
+<button id="buttonNo" on:click={() => (value = 'no')}>button-no</button>
