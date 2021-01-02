22e3f599d3e1f186325021a928410d18fdf5a8c8
koory1st <32436334@qq.com>
2021-01-02 19:47:52 +0800

    test(checkboxgroup): modify test

--- __test__/SeuCheckboxGroup/CheckboxGroup.click.svelte
+++ __test__/SeuCheckboxGroup/CheckboxGroup.click.svelte
@@ -8,10 +8,3 @@
   <span>{group}</span>
   <SeuCheckboxGroup options={['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']} bind:group />
 </div>
-
-<button on:click={() => (group = [])}>buttonEmpty</button>
-<button on:click={() => (group = ['Shanghai'])}>buttonShanghai</button>
-<button on:click={() => (group = ['Beijing'])}>buttonBeijing</button>
-<button on:click={() => (group = ['Guangzhou'])}>buttonGuangzhou</button>
-<button on:click={() => (group = ['Shenzhen'])}>buttonShenzhen</button>
-<button on:click={() => (group = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen'])}>buttonAll</button>
