449a189adade0e5ccfaf4acde47b45a8438fd865
koory1st <32436334@qq.com>
2021-01-07 19:48:49 +0800

    test(checkbox): change border file name

--- __test__/SeuCheckbox/border.test.js
+++ __test__/SeuCheckbox/border.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/svelte'
 import { testElementAndResult } from '../util/TestUtil'
-import Comp from './Checkbox.border.svelte'
+import Comp from './border.svelte'
 
 test('border', () => {
   const { getByText } = render(Comp)
