3610ec3a12feacadac0f1414a36fd80ae1a543ae
koory1st <32436334@qq.com>
2021-01-07 19:55:40 +0800

    test(checkbox): change change event file name

--- __test__/SeuCheckbox/changeEvent.test.js
+++ __test__/SeuCheckbox/changeEvent.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/svelte'
 import { testElementAndResult } from '../util/TestUtil'
-import Comp from './Checkbox.changeEvent.svelte'
+import Comp from './changeEvent.svelte'
 
 test('change the value by click checkbox', async () => {
   const { getByText } = render(Comp)
