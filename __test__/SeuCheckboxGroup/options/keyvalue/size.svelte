5b0272fd4871416cc43bc0234ba2b9963c590679
koory1st <32436334@qq.com>
2021-01-13 23:07:28 +0800

    test(checkbox group): add size test

---
+++ __test__/SeuCheckboxGroup/options/keyvalue/size.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+  import SeuCheckboxGroup from '../../../../src/checkbox/SeuCheckboxGroup.svelte'
+</script>
+
+<div>
+  test component
+  <SeuCheckboxGroup
+    size="medium"
+    options={[{ label: 'Shanghai', value: 1 }, { label: 'Beijing', value: 2 }, { label: 'Guangzhou', value: 3 }]} />
+  <SeuCheckboxGroup
+    border
+    size="medium"
+    options={[{ label: 'Shanghai', value: 1 }, { label: 'Beijing', value: 2 }, { label: 'Guangzhou', value: 3 }]} />
+  <SeuCheckboxGroup
+    border
+    size="small"
+    options={[{ label: 'Shanghai', value: 1 }, { label: 'Beijing', value: 2 }, { label: 'Guangzhou', value: 3 }]} />
+  <SeuCheckboxGroup
+    border
+    size="mini"
+    options={[{ label: 'Shanghai', value: 1 }, { label: 'Beijing', value: 2 }, { label: 'Guangzhou', value: 3 }]} />
+</div>
