24b9d2d8328b5d71aa564d157418efadd7394289
koory1st <32436334@qq.com>
2021-01-13 23:03:44 +0800

    test(checkbox group): add border test

---
+++ __test__/SeuCheckboxGroup/options/keyvalue/border.svelte
@@ -0,0 +1,10 @@
+<script type="ts">
+  import SeuCheckboxGroup from '../../../../src/checkbox/SeuCheckboxGroup.svelte'
+</script>
+
+<div>
+  test component
+  <SeuCheckboxGroup
+    border
+    options={[{ label: 'Shanghai', value: 1 }, { label: 'Beijing', value: 2 }, { label: 'Guangzhou', value: 3 }]} />
+</div>
