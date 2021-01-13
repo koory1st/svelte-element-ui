f41a92934ead4f4c35f1c2ded59c9baef52181f7
koory1st <32436334@qq.com>
2021-01-13 21:56:42 +0800

    test(radio): add slot's change event test

---
+++ __test__/SeuRadioGroup/slot/radio/changeValue.svelte
@@ -0,0 +1,19 @@
+<script type="ts">
+  import SeuRadioGroup from '../../../../src/radio/SeuRadioGroup.svelte'
+  import SeuRadio from '../../../../src/radio/SeuRadio.svelte'
+  let value
+  let changeValue
+  function changeHandler({ detail }) {
+    changeValue = detail
+  }
+</script>
+
+<div>
+  test component
+  <span>changeValue: {changeValue}</span>
+  <SeuRadioGroup on:change={changeHandler}>
+    <SeuRadio bind:value label={'1'}>label1</SeuRadio>
+    <SeuRadio bind:value label={'2'}>label2</SeuRadio>
+    <SeuRadio bind:value label={'3'}>label3</SeuRadio>
+  </SeuRadioGroup>
+</div>
