ca571696c85cffe99d890753168e9270a23edc4e
koory1st <32436334@qq.com>
2021-01-13 21:47:22 +0800

    test(radio): add slot boolean value test

---
+++ __test__/SeuRadioGroup/slot/radio/value.boolean.svelte
@@ -0,0 +1,16 @@
+<script type="ts">
+  import SeuRadioGroup from '../../../../src/radio/SeuRadioGroup.svelte'
+  import SeuRadio from '../../../../src/radio/SeuRadio.svelte'
+  let value
+</script>
+
+<div>
+  test component
+  <span> value: {value} </span>
+  <SeuRadioGroup>
+    <SeuRadio bind:value label={true}>label1</SeuRadio>
+    <SeuRadio bind:value label={false}>label2</SeuRadio>
+  </SeuRadioGroup>
+</div>
+<button on:click={() => (value = true)}>button01</button>
+<button on:click={() => (value = false)}>button02</button>
