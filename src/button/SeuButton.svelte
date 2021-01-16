615fc6017281f403dbc3775be6cb5e8b40e9543e
koory1st <32436334@qq.com>
2021-01-16 15:17:38 +0800

    refactor(button): change class function

--- src/button/SeuButton.svelte
+++ src/button/SeuButton.svelte
@@ -1,4 +1,5 @@
 <script type="ts">
+  import { getClass } from '../util/StringUtil'
   export let type = 'default'
   export let size = ''
   export let icon = ''
@@ -9,36 +10,19 @@
   export let round = false
   export let circle = false
 
-  let classList = ['seu-button']
-
-  $: if (disabled) {
-    classList.push('is-disabled')
-  }
-
-  $: if (plain) {
-    classList.push('is-plain')
-  }
-
-  classList.push(`seu-button--${type || 'default'}`)
-
-  $: if (round) {
-    classList.push('is-round')
-  }
-
-  $: if (circle) {
-    classList.push('is-circle')
-  }
-
-  $: if (size) {
-    classList.push(`seu-button--${size}`)
-  }
-
-  $: if (loading) {
-    classList.push('is-loading')
-  }
+  $: classString = getClass([
+    'seu-button',
+    `seu-button--${type || 'default'}`,
+    [`is-disabled`, disabled],
+    [`is-plain`, plain],
+    [`is-round`, round],
+    [`is-circle`, circle],
+    [`seu-button--${size}`, Boolean(size)],
+    ['is-loading', loading],
+  ])
 </script>
 
-<button class={classList.join(' ')} type={nativeType} disabled={disabled || loading} on:click>
+<button class={classString} type={nativeType} disabled={disabled || loading} on:click>
   {#if loading}<i class="seu-icon-loading" />{/if}
   {#if icon && !loading}<i class={icon} />{/if}
   {#if $$slots.default}
