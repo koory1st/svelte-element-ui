8792609e554a6b4010e271154b97c1b4c29d3e31
koory1st <32436334@qq.com>
2021-04-18 16:19:25 +0800

    docs(site): change site's vite config, add repo name

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,5 +3,10 @@ import svelte from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/svelte-element-ui/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 })
