553180ae471ba5aa9e1068fd96a871948f60d8ff
koory1st <32436334@qq.com>
2021-04-24 18:19:03 +0800

    fix(popperjs): change import popperjs

--- packages/util/SveltePopperJs.ts
+++ packages/util/SveltePopperJs.ts
@@ -2,7 +2,7 @@
  * origin code
  * https://github.com/bryanmylee/svelte-popperjs
  */
-import { createPopper } from '@popperjs/core'
+import { createPopper } from '@popperjs/core/dist/umd/popper.min.js'
 
 function createPopperActions(initOptions: any) {
   var popperInstance = null
