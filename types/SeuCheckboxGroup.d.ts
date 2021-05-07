9deb4a1805bb62d0dfefaa47a1b68a4d6c997cbd
koory1st <32436334@qq.com>
2021-05-07 21:46:05 +0800

    fix(checkbox): add types

--- types/SeuCheckboxGroup.d.ts
+++ types/SeuCheckboxGroup.d.ts
@@ -1,6 +1,6 @@
 import type { SvelteComponentTyped } from 'svelte'
-import { IChangeEvent, IOptionsProp, IGroupProp, IBorderProp, ISizeProp, IDisabledProp, ITypeProp } from './Types'
+import { IChangeEvent, IOptionsProp, IGroupProp, IBorderProp, ISizeProp, IDisabledProp, ITypeProp, IMaxProp, IMinProp } from './Types'
 
-interface IProps extends IOptionsProp, IGroupProp, IBorderProp, ISizeProp, IDisabledProp, ITypeProp {}
+interface IProps extends IOptionsProp, IGroupProp, IBorderProp, ISizeProp, IDisabledProp, ITypeProp, IMaxProp, IMinProp {}
 
 export class SeuCheckboxGroup extends SvelteComponentTyped<IProps, IChangeEvent> {}
