0dc8590b1812d859768a3ed1204f7a7fa0fcb929
koory1st <32436334@qq.com>
2021-05-08 23:24:24 +0800

    chore(checkbox group): add example

---
+++ examples/checkboxGroup/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { SeuCheckboxGroup } from '../../src/index'
+  const cityOptions = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']
+  let checkboxGroup1 = ['Shanghai']
+  let checkboxGroup2 = ['Shanghai']
+  let checkboxGroup3 = ['Shanghai']
+  let checkboxGroup4 = ['Shanghai']
+</script>
+
+<div>
+  {checkboxGroup1}
+  <SeuCheckboxGroup bind:group={checkboxGroup1} options={cityOptions} type="button" />
+</div>
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Menu from '../examples/checkbox/App.svelte'
+  import Menu from '../examples/checkboxGroup/App.svelte'
 </script>
 
 <Menu />
